61911231017539ac17f3102acc362bb44ffd5b6e
manishverma <manishverma25@gmail.com>
2021-10-07 23:21:36 +0530

    temp left file

--- app/src/main/java/com/saifi369/daggerkotlinexample/Car.kt
+++ app/src/main/java/com/saifi369/daggerkotlinexample/Car.kt
@@ -1,12 +1,21 @@
 package com.saifi369.daggerkotlinexample
 
-class Car (var engine: Engine,
-           var battery: Battery) {
+import javax.inject.Inject
+
+class Car @Inject constructor(
+    var engine: Engine,
+    var battery: Battery
+) {
 
     fun drive() {
         battery.ignite()
         engine.start()
         Utils.showTag("Vhrooom! Car is driving")
+    }
+
+    @Inject
+    fun attachToLicensePlate(licensePlate: LicensePlate) {
+        licensePlate.attachToCar(this)
     }
 
 }
